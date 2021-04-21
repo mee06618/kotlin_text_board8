8cb5bc8ab9df03671411593fe8ec04cc5ce94f58
mee06618 <bro89977@naver.com>
2021-04-21 12:56:30 +0900

    post

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,64 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+fun readLineTrim() = readLine()!!.trim()
+
 fun main() {
-    print("숫자 개수 : ")
-    var num:Int = readLine()!!.trim().toInt()
-    println("${num}개의 숫자를 입력 받습니다")
-    var arr: Array<Int> = Array<Int>(num,{0})
-    for(i in arr.indices){
-        print("${i+1}번째 숫자 : ")
-        arr[i]= readLine()!!.trim().toInt()
+    println("== 게시판 관리 프로그램 시작 ==")
+
+    // 가장 마지막에 입력된 게시물 번호
+    var articlesLastId = 0
+
+    val articles = mutableListOf<Article>()
+
+    loop@ while (true) {
+        print("명령어) ")
+        val command = readLineTrim()
+
+        when (command) {
+            "system exit" -> {
+                println("프로그램을 종료합니다.")
+                break@loop
+            }
+            "article write" -> {
+                val id = articlesLastId + 1
+                print("제목 : ")
+                val title = readLineTrim()
+                print("내용 : ")
+                val body = readLineTrim()
+                val reg = LocalDateTime.now()
+                val up = LocalDateTime.now()
+                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                val forReg=reg.format((formatter))
+                val forUp=up.format((formatter))
+                val article = Article(id, title, body,forReg,forUp)
+
+                println("${id}번 게시물이 작성되었습니다.")
+
+                articles.add(article)
+
+                articlesLastId = id
+            }
+            "article list" -> {
+                println("번호 / 제목")
+
+                for ( article in articles ) {
+                    println("${article.id} / ${article.title} / ${article.body} / ${article.regDate} / ${article.updateDate}")
+                }
+            }
+            else -> {
+                println("`$command` 은(는) 존재하지 않는 명령어 입니다.")
+            }
+        }
     }
-    println("입력하신 숫자의 합은 : ${arr.sum()}")
-    println("입력하신 숫자의 합은 : ${arr.average()}")
+
+    println("== 게시판 관리 프로그램 끝 ==")
 }
+
+data class Article(
+    val id: Int,
+    val title: String,
+    val body: String,
+    val regDate:String,
+    val updateDate:String
+)
